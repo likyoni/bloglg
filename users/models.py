676eaa03861cdfc20c3979634628d7555a90bbff
likyoni <prakharrastogi46@gmail.com>
2025-07-25 14:15:17 +0530

    first commit

---
+++ users/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img_path = self.image.path
+        try:
+            img = Image.open(img_path)
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(img_path)
+        except Exception as e:
+            # Optional: add logging here in real projects
+            print(f"Error resizing image: {e}")
